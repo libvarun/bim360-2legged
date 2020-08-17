e6b8db0b016e7922d1cf45bcc0115cfa511b3f5e
varun patil <varun.patil578@gmail.com>
2020-08-17 13:23:36 +0530

    initial build

---
+++ public/js/ForgeTree.js
@@ -0,0 +1,21 @@
+﻿/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+$(document).ready(function () {
+  launchViewer('dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLjV2RE02SkNXUV9hNUxRcTdBNno4Unc/dmVyc2lvbj0x');
+});
